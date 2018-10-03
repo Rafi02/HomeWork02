c3e701457b29f09a723c7d6463b856b27c38253b
Rafi02 <rafborowiec@gmail.com>
2018-10-03 19:16:14 +0200

    HomeWork after class 2

--- src/main/java/com/example/Discount.java
+++ src/main/java/com/example/Discount.java
@@ -1,25 +1,29 @@
 package com.example;
 
-import java.sql.SQLOutput;
+        import java.sql.SQLOutput;
 
 public class Discount {
     public static void main(String[] args) {
         int age = 40;
         boolean isVip = true;
-        System.out.println("discount = " + getDiscount(age, isVip));
+        System.out.println("discount = " + getDiscount(10, false));
         System.out.println("discount = " + getDiscount(10, true));
+        System.out.println("discount = " + getDiscount(20, false));
+        System.out.println("discount = " + getDiscount(20, true));
         System.out.println("discount = " + getDiscount(70, false));
+        System.out.println("discount = " + getDiscount(70, true));
+
     }
-private static int getDiscount (int age, boolean isVip) {
-    int discount = 0;
-    if (age > 65)   {
-        discount = 10;
-    } else if (age < 18) {
-        discount = 25;
-    }
-    if (isVip) {
-        discount = discount + 5;
-    }
+    private static int getDiscount (int age, boolean isVip) {
+        int discount = 0;
+        if (age > 65)   {
+            discount = 10;
+        } else if (age < 18) {
+            discount = 25;
+        }
+        if (isVip) {
+            discount = discount + 5;
+        }
         return discount;
-}
+    }
 }
